013322a79ac64b34936c2825aecac56d5441bf2a
Oleh <olehtomas@gmail.com>
2015-11-02 23:59:42 +0200

    Update first.cpp

--- Softheme/first.cpp
+++ Softheme/first.cpp
@@ -1,8 +1,21 @@
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main(){
+  fstream in("input.txt", ios::in), out("output.txt", ios::out);
 
-
+  int ans = 0, cur = 0;
+  string s;
+  in >> s;
+  for (int i = 0; i < n; i++){
+    if (s[i] == '0'){
+      cur++;
+      ans = max(ans, cur);
+    }
+    else cur = 0;
+  }
+  
+  out << ans << endl;
 }
